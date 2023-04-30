8ec612bd96abee250b749cbe274ead30dfd9a44f
DainWs <jose.ant.du@gmail.com>
2023-04-30 13:41:44 +0200

    Updating project

---
+++ crm-view-nodejs/src/vue/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { RouterView } from 'vue-router.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RouterView
+  }
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style> 
+html, body, #app, main {
+  width: 100%;
+  height: 100%;
+}
+</style>
