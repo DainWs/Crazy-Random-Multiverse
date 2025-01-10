8844f6b2d78ee2f93ddf3ecbd749acb73865443d
DainWs <jose.ant.du@gmail.com>
2025-01-10 16:52:22 +0100

    AI only select card to attack once fix

--- src/main/java/com/dainws/games/crm/domain/ai/score/ScoreBasedActionManager.java
+++ src/main/java/com/dainws/games/crm/domain/ai/score/ScoreBasedActionManager.java
@@ -3,6 +3,7 @@ package com.dainws.games.crm.domain.ai.score;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import com.dainws.games.crm.domain.ai.AIAction;
 import com.dainws.games.crm.domain.ai.AIActionTemplate;
@@ -12,18 +13,19 @@ import com.dainws.games.crm.domain.core.action.Action;
 import com.dainws.games.crm.domain.core.action.ActionContext;
 import com.dainws.games.crm.domain.core.action.AttackAction;
 import com.dainws.games.crm.domain.core.action.PutAction;
+import com.dainws.games.crm.domain.core.action.UseSpellAction;
 
 public class ScoreBasedActionManager extends BaseActionManager {
 
-	private Map<Class<? extends Action>, ActionContextFactory> actionContextFactories;
+	private Map<Class<? extends Action>, Supplier<? extends ActionContextFactory>> actionContextFactories;
 
 	public ScoreBasedActionManager() {
 		this.actionContextFactories = new HashMap<>();
-		this.actionContextFactories.put(AttackAction.class, new ScoreAttackContextFactory());
-		this.actionContextFactories.put(PutAction.class, new ScorePutContextFactory());
+		this.actionContextFactories.put(AttackAction.class, ScoreAttackContextFactory::new);
+		this.actionContextFactories.put(PutAction.class, ScorePutContextFactory::new);
 	}
 
-	public void setContextFactory(Class<? extends Action> actionType, ActionContextFactory factory) {
+	public void setContextFactory(Class<? extends Action> actionType, Supplier<? extends ActionContextFactory> factory) {
 		this.actionContextFactories.put(actionType, factory);
 	}
 
@@ -33,7 +35,7 @@ public class ScoreBasedActionManager extends BaseActionManager {
 			return List.of();
 		}
 
-		ActionContextFactory contextFactory = this.actionContextFactories.get(template.getActionType());
+		ActionContextFactory contextFactory = this.actionContextFactories.get(template.getActionType()).get();
 		ActionContext actionContext = contextFactory.decideContextFor(context, template);
 		return List.of(template.createAction(actionContext));
 	}
