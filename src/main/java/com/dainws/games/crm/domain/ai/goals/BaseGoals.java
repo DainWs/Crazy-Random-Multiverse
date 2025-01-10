9bd3a7c4b9a0498d3ce3d31991eb41fa30d7950e
DainWs <jose.ant.du@gmail.com>
2025-01-10 16:50:39 +0100

    AI - Generalization of specific action conditions

--- src/main/java/com/dainws/games/crm/domain/ai/goals/BaseGoals.java
+++ src/main/java/com/dainws/games/crm/domain/ai/goals/BaseGoals.java
@@ -1,34 +1,36 @@
 package com.dainws.games.crm.domain.ai.goals;
 
 import com.dainws.games.crm.domain.ai.AIPlayer;
-import com.dainws.games.crm.domain.ai.goals.conditions.AttackActionArchiveCondition;
-import com.dainws.games.crm.domain.ai.goals.conditions.PutActionArchiveCondition;
+import com.dainws.games.crm.domain.ai.goals.conditions.SpecificActionArchiveCondition;
 import com.dainws.games.crm.domain.ai.goals.conditions.TargetPlayerDecorator;
+import com.dainws.games.crm.domain.core.action.AttackAction;
+import com.dainws.games.crm.domain.core.action.PutAction;
+import com.dainws.games.crm.domain.core.action.UseSpellAction;
 import com.dainws.games.crm.domain.core.player.Player;
 
 public class BaseGoals {
 	private BaseGoals() {}
 	
 	public static Goal createPutAllCards(Player targetPlayer) {
-		GoalArchiveCondition archiveCondition = new PutActionArchiveCondition();
+		GoalArchiveCondition archiveCondition = new SpecificActionArchiveCondition(PutAction.class);
 		GoalArchiveCondition isTargetPlayer = new TargetPlayerDecorator(archiveCondition, targetPlayer);
 		return new InfiniteGoal(BaseGoalNames.PUT_CARD, isTargetPlayer);
 	}
 	
 	public static Goal createAttackWithAllCardsToPlayer(Player targetPlayer) {
-		GoalArchiveCondition archiveCondition = new AttackActionArchiveCondition();
+		GoalArchiveCondition archiveCondition = new SpecificActionArchiveCondition(AttackAction.class);
 		GoalArchiveCondition isTargetPlayer = new TargetPlayerDecorator(archiveCondition, targetPlayer);
 		return new InfiniteGoal(BaseGoalNames.ATTACK_PLAYER, isTargetPlayer);
 	}
 	
 	public static Goal createAttackPlayerOneTime(Player targetPlayer) {
-		GoalArchiveCondition archiveCondition = new PutActionArchiveCondition();
+		GoalArchiveCondition archiveCondition = new SpecificActionArchiveCondition(AttackAction.class);
 		GoalArchiveCondition isTargetPlayer = new TargetPlayerDecorator(archiveCondition, targetPlayer);
 		return new OneTimeGoal(BaseGoalNames.ATTACK_PLAYER, isTargetPlayer);
 	}
 	
 	public static Goal createAttackPlayerNTimes(AIPlayer targetPlayer, int times) {
-		GoalArchiveCondition archiveCondition = new PutActionArchiveCondition();
+		GoalArchiveCondition archiveCondition = new SpecificActionArchiveCondition(AttackAction.class);
 		GoalArchiveCondition isTargetPlayer = new TargetPlayerDecorator(archiveCondition, targetPlayer);
 		return new NTimesGoal(BaseGoalNames.ATTACK_PLAYER, isTargetPlayer, times);
 	}
