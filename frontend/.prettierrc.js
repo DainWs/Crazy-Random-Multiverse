197247cf081178fd2c1139168520f203e97866bd
DainWs <jose.ant.du@gmail.com>
2025-06-21 13:16:47 +0200

    Testing frontend with phaser

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: 'none',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  printWidth: 160,
+  singleAttributePerLine: true
+};
