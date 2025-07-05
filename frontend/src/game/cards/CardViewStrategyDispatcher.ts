d06ad26e005ae5bb265093d2b680003dc22ccdfe
DainWs <jose.ant.du@gmail.com>
2025-07-05 15:23:38 +0200

    Equipment view strategy

--- frontend/src/game/cards/CardViewStrategyDispatcher.ts
+++ frontend/src/game/cards/CardViewStrategyDispatcher.ts
@@ -1,10 +1,11 @@
 import Card from "@/domain/Card";
 import CombatantViewStrategy from "@/game/cards/CombatantViewStategy";
+import EquipmentViewStrategy from "@/game/cards/EquipmentViewStrategy";
 import SpellViewStrategy from "@/game/cards/SpellViewStrategy";
 
 const combatantViewStrategy = new CombatantViewStrategy();
 const spellViewStrategy = new SpellViewStrategy();
-const equipmentViewStrategy = new CombatantViewStrategy();
+const equipmentViewStrategy = new EquipmentViewStrategy();
 
 function dispatchCardViewStrategy(card: Card) {
   if (card.isType("EQUIPMENT")) return equipmentViewStrategy;
